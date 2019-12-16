9eb3085e04c06940874586aee2887f55f3907d4b
Jakob Walsh <Jakob.Walsh12@sky.com>
2019-12-16 14:29:24 +0000

    Uploading files

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
 import React from 'react';
-import first from './images/images1.jpeg';
-import second from './images/images2.jpeg';
-import third from './images/images3.jpeg';
-import fourth from './images/images4.jpeg';
-import fifth from './images/images5.jpeg';
-import sixth from './images/images6.jpeg';
-
-import Card from './Card';
 import './App.css';
-
-
-
-class App extends React.Component  {
-  render(){
+class App extends React.Component {
+  state = {number:0}
+  increment = () => {
+    this.setState({number: this.state.number+=1})
+    console.log(this.state.number)
+    return(this.state.number)
+  }
+  decrement = () => {
+    this.setState({number: this.state.number-=1})
+    console.log(this.state.number)
+    return(this.state.number)
+  }
+  render () {
     return(
-      <div className="App">
-        <div>
-          <h3>News Headlines</h3>
-          <div>
-            <Card pic = {first} text = "Planet" topic = "Whats for lunch"/>
-            <Card pic = {second} text = "Dog" topic = "World hunger"/>
-            <Card pic = {third} text = "Pokemon" topic = "POKEMON"/>
-          
-          </div>
-        </div>
-
-        <div>
-          <h3>Sports Headlines</h3>
-          <div>
-          <Card pic = {fourth} text = "Crouch" topic = "Crouchy forcasts weather"/>
-            <Card pic = {fifth} text = "Ball" topic = "GOAL!!!"/>
-            <Card pic = {sixth} text = "Bridge" topic = "On water"/>
-
-           
-          </div>
-        </div>
+      <div>
+        <button onClick = {this.increment}>Increase</button>
+        <button onClick = {this.decrement}>Decrease</button>  
+        <h1>Your number is {this.state.number}</h1>    
       </div>
-    )
+      )
   }
 }
-
-
-
 export default App;
+
+
+
+
+
+
+
